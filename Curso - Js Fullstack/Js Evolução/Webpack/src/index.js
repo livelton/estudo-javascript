0461294a6f664ecb0b5309a94cb3d8d550b3ff55
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-10-17 11:09:05 -0300

    Aplicando o webpack

---
+++ Curso - Js Fullstack/Js Evolução/Webpack/src/index.js
@@ -0,0 +1,7 @@
+import moment from "moment/src/moment"
+
+alert(moment().format("HH:mm:ss"))
+
+//o webpack irá gerar um arquivo, tirar espaço e otimizar o processamento
+//criará um main no index.html
+
