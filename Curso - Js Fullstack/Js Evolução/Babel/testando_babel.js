d349302109fcf02f0842329852a388a4adfb66e6
livelton <elivelton.pereira@academico.ifpb.edu.br>
2022-10-17 10:35:22 -0300

    Praticando o Babel

---
+++ Curso - Js Fullstack/Js Evolução/Babel/testando_babel.js
@@ -0,0 +1,8 @@
+function testandoBabel(texto1, texto2,){
+    alert(`${texto1}\n ${texto2}`);
+}
+
+//através do npx babel .\testando_babel.js 
+//a vírgula no último parâmetro foi removida, pois algumas funções do novo es não funcionam nos es antigos, então o babel corrigiu esse "erro" no script compilado
+
+//OSERVE O SCRIPT COMPILADO
